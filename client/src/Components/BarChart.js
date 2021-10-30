1f9cb40fe036f1c4acbfa04e45ba16b44ce544fb
James <jamesmca@live.com>
2021-10-30 13:44:11 -0700

    Update BarChart.js
    risk v return chart functional

--- client/src/Components/BarChart.js
+++ client/src/Components/BarChart.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Bar } from 'react-chartjs-2';
 
-export default function BarChart({data}) {
+export default function BarChart({ data }) {
     let risky = data.getRiskReward
+    let aloha = risky.sort
 
-    console.log('risky', risky)
-    const labels = risky.map(label => label.name.replace('_', ' ').toLowerCase()).slice(0,15)
-    const risk = risky.map(label => label.risk).slice(0,15)
-    const reward = risky.map(label => label.reward).slice(0,15)
+    console.log('risky', aloha)
+    const labels = risky.map(label => label.name.replaceAll('_', ' ')).slice(-15)
+    const risk = risky.map(label => label.risk).slice(-15)
+    const reward = risky.map(label => label.reward).slice(-15)
+    console.log(risk)
     return (
         <Bar
             data={{
                 labels: labels,
                 datasets: [
                     {
-                        label: 'Average Successful Regonition Per Class',
+                        label: 'Return',
+                        yAxisID: 'A',
                         data: reward,
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(0, 200, 50, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
                             'rgba(255, 206, 86, 0.2)',
                             'rgba(75, 192, 192, 0.2)',
@@ -25,7 +28,7 @@ export default function BarChart({data}) {
                             'rgba(255, 159, 64, 0.2)'
                         ],
                         borderColor: [
-                            'rgba(255,99,132,1)',
+                            'rgba(0, 200, 50, 1)',
                             'rgba(54, 162, 235, 1)',
                             'rgba(255, 206, 86, 1)',
                             'rgba(75, 192, 192, 1)',
@@ -33,8 +36,35 @@ export default function BarChart({data}) {
                             'rgba(255, 159, 64, 1)'
                         ],
                         borderWidth: 1,
+                    },
+                    {
+                        label: 'Risk',
+                        yAxisID: 'B',
+                        data: risk,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                        ],
+                        borderWidth: 1,
                     }
-                ]
+
+                ],
+                options: {
+                    scales: {
+                        A: {
+                            type: 'linear',
+                            position: 'left',
+
+                        },
+                        B: {
+                            type: 'linear',
+                            position: 'right',
+
+                        }
+                    }
+                  }
             }}
             height={400}
             width={600}
